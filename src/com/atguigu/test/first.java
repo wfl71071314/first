15236b8a23bebe0cbf955d5ef8a2b45fcee4051c
wfl71071314 <490000926@qq.com>
2016-07-08 19:58:02 +0800

    调试

--- src/com/atguigu/test/first.java
+++ src/com/atguigu/test/first.java
@@ -5,5 +5,7 @@ public class first {
 		System.out.println("111111");
 		System.out.println("222222");
 		System.out.println("aaaaaa");
+		System.out.println("333333");
+		System.out.println("bbbbbb");
 	}
 }
